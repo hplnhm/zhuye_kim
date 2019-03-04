b36b18f48476a91248c33e6096513df2ec97b347
hplnhm <dayu.zhang@postgrad.mbs.ac.uk>
2019-03-05 02:26:44 +0800

    Update settings.py

--- zhuye_kim/settings.py
+++ zhuye_kim/settings.py
@@ -69,7 +69,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'zhuye',
-        'USER': 'iyangren_com',
+        'USER': 'root',
         'PASSWORD': 'Dayu0215',
         'HOST': '127.0.0.1',
         'PORT': '3306',
